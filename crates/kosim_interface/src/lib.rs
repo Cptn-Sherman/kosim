576498ee3fe90d2073b017a391de4933150211df
Captain Sherman <cptn.sherman@gmail.com>
2026-03-21 00:43:42 -0400

    formatting

--- crates/kosim_interface/src/lib.rs
+++ crates/kosim_interface/src/lib.rs
@@ -1,10 +1,14 @@
-use bevy::{asset::Handle, color::Color, ecs::bundle::Bundle, text::{Font, FontFeatures, FontSmoothing, FontWeight, TextColor, TextFont}, ui::widget::Text};
-
+use bevy::{
+    asset::Handle,
+    color::Color,
+    ecs::bundle::Bundle,
+    text::{Font, FontFeatures, FontSmoothing, FontWeight, TextColor, TextFont},
+    ui::widget::Text,
+};
 
 pub const DEFAULT_FONT_PATH: &str = "fonts/AshlanderPixel_fixed.ttf";
 pub const DEFAULT_DEBUG_FONT_PATH: &str = "fonts/Monocraft.ttf";
 pub const DEFAULT_FONT_SIZE: f32 = 14.0;
-
 
 #[allow(dead_code)]
 pub const ORANGE_TEXT_COLOR: Color = Color::hsv(0.34, 1.0, 0.5);
@@ -13,9 +17,8 @@ pub const YELLOW_GREEN_TEXT_COLOR: Color = Color::hsv(0.9, 0.69, 0.58);
 #[allow(dead_code)]
 pub const RED_TEXT_COLOR: Color = Color::srgb(1.0, 0.0, 0.0);
 #[allow(dead_code)]
-pub const GOLD_TEXT_COLOR: Color = Color::srgb(1.0 , 0.72, 0.0);
-pub const BORDER_COLOR: Color = Color::srgb(0.8 , 0.8, 0.8);
-
+pub const GOLD_TEXT_COLOR: Color = Color::srgb(1.0, 0.72, 0.0);
+pub const BORDER_COLOR: Color = Color::srgb(0.8, 0.8, 0.8);
 
 #[allow(dead_code)]
 pub fn gen_text_section(
@@ -23,7 +26,7 @@ pub fn gen_text_section(
     size: Option<f32>,
     color: Option<Color>,
     font: Handle<Font>,
-) -> impl Bundle { 
+) -> impl Bundle {
     (
         Text::new(value.unwrap_or_default()),
         TextFont {
